c63bebbb895cf7af3d27b8749edb2bf6eb4a5712
DennisDv24 <jokarboxing@gmail.com>
2021-05-08 16:57:36 +0200

    v1.0 The system is ready for any elimination rule or for introduction rules/theorems that can't      cause infinite introductions

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from GentzenForest import *
+from GentzenForest import NonMonadicGentzenForest, inferences, logic
 
 premises = logic.Premises(['( not ((p and q) or (q -> r)))', '(p or q)'])
 premises.append('(p -> (a and b))')
 premises.append('(q -> (a and b))')
 
 GF = NonMonadicGentzenForest(premises)
-Morgan1 = NonMonadicInference('( not (X or Y) )', '(( not X ) and ( not Y))')
-Morgan2 = NonMonadicInference('( not (X and Y) )', '(( not X ) or ( not Y))')
+Morgan1 = inferences.NonMonadicInference('( not (X or Y) )', '(( not X ) and ( not Y))')
+Morgan2 = inferences.NonMonadicInference('( not (X and Y) )', '(( not X ) or ( not Y))')
 
 GF.add_inference(Morgan1)
 GF.add_inference(Morgan2)
